028dbb3cc3216921ff091c9205177e25639f6709
Eemeli <eemeli.surakka@hotmail.fi>
2019-09-16 01:20:33 +0300

    Merge problems fixed

--- navigation/pages/NewDream.js
+++ navigation/pages/NewDream.js
@@ -23,36 +23,4 @@ const styles = StyleSheet.create({
     }
 });
 
-
-=======
-import React, { Component } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
-import { Header, Left, Right, Body, Title } from 'native-base';
-
-import NewDreamForm from './components/NewDreamForm/NewDreamForm';
-
-class NewDream extends Component {
-
-    render() {
-        return (
-            <View style={styles.container}>
-
-                <NewDreamForm style={styles.dreamForm} />
-
-            </View>
-        );
-    }
-}
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
-    dreamForm: {
-        padding: 25,
-    }
-});
-
-
->>>>>>> af871cc0dc0309881efbb36aa83df4b85bda31c7
 export default NewDream;
--- navigation/pages/components/DreamList/SingleDream/SingleDream.js
+++ navigation/pages/components/DreamList/SingleDream/SingleDream.js
@@ -40,48 +40,4 @@ const styles = StyleSheet.create({
   }
 });
 
-=======
-import React from 'react';
-import { View, StyleSheet, TouchableWithoutFeedback } from 'react-native';
-import { Container, Header, Content, Card, CardItem, Text, Body } from "native-base";
-
-
-const listItem = ( props ) => (
-  <TouchableWithoutFeedback onPress={props.onDreamPressed}>
-    <View style={styles.listItem}>
-
-      <Text style={styles.name}>{props.name}</Text>
-
-      <Text style={styles.dreamsign}>{props.dreamsign}</Text>
-      <Text style={styles.date}>{props.date}</Text>
-
-    </View>
-  </TouchableWithoutFeedback>
-
-);
-
-const styles = StyleSheet.create({
-  listItem: {
-    backgroundColor: 'rgba(52, 52, 52, 0.4)',
-    alignSelf: 'center',
-    alignItems: 'center',
-    width: "80%",
-    marginBottom: 5,
-    padding: 15,
-  },
-  name: {
-    color: 'salmon',
-    fontSize: 20,
-    borderColor: 'grey',
-  },
-  dreamsign: {
-    color: 'white',
-    padding: 3,
-  },
-  date: {
-    color: '#DBD7CF',
-  }
-});
-
->>>>>>> af871cc0dc0309881efbb36aa83df4b85bda31c7
 export default listItem;
